18296801c9dd0c173c8d0823a05f82fe7acdd912
izklch <53634565+izklch@users.noreply.github.com>
2019-08-27 14:34:05 +0700

    Update planet.js

--- assets/planet.js
+++ assets/planet.js
@@ -1,6 +1,11 @@
 var request = new XMLHttpRequest()
 
 request.open('GET', 'https://swapi.co/api/planets/', true)
+request.onerror = function(){
+  document.querySelector("#loading-indicator h5").textContent = "Connection Error";
+  document.querySelector("#loading-indicator h6").textContent = "Please Try Again";
+  console.log('error')
+}
 request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
@@ -8,8 +13,15 @@ request.onload = function() {
   var row
   var cell
   if (request.status >= 200 && request.status < 400) {
-    for(var i in data.results) {
+    // data.results is an array, avoid using for..in loop:
+    // for(var i in data.results) {
+    for(var i=0; i < data.results.length; i++) {
       row = table.insertRow(-1);
+      if(i%2!==0){
+        row.className = "bg-info"
+      } else {
+        row.className = "bg-primary"
+      }
       cell = row.insertCell(-1);
       cell.innerHTML = data.results[i].name
       cell = row.insertCell(-1);
@@ -29,7 +41,10 @@ request.onload = function() {
       cell = row.insertCell(-1);
       cell.innerHTML = data.results[i].population
     }
+    document.querySelector("#loading-indicator").remove()
   } else {
+    document.querySelector("#loading-indicator h5").textContent = "Connection Error";
+    document.querySelector("#loading-indicator h6").textContent = "Please Try Again";
     console.log('error')
   }
 }
